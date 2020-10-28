e062267af75c46bb7c46713472be9b07ba9be76a
Santosh Mohanty <santa-1987@gmail.com>
2020-10-28 20:42:19 +0530

    Added Initial Files

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground style={ styles.imgBackground } 
+                 resizeMode='cover' 
+                 source={require('./assets/gullu.jpg')}>
+      <Text>Welcome to Grocery Expense list!..........</Text>
       <StatusBar style="auto" />
+     </ImageBackground>
     </View>
   );
 }
@@ -17,5 +23,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imgBackground: {
+    width: '100%',
+    height: '100%',
+    flex: 2 
   },
 });
