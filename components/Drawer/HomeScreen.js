d70f79354ac65ed786f3ede652f89d55afdf0802
Santosh Mohanty <Santosh.Mohanty@cerner.com>
2020-12-07 19:06:01 +0530

    Allow Contacts

--- components/Drawer/HomeScreen.js
+++ components/Drawer/HomeScreen.js
@@ -3,52 +3,61 @@
 
 // Import React and Component
 import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+} from "react-native";
+import * as Contacts from "expo-contacts";
 
 const HomeScreen = () => {
+  const [contacts, setContacts] = React.useState([]);
+  React.useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === "granted") {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.PHONE_NUMBERS, Contacts.EMAILS],
+        });
+
+        if (data.length > 0) {
+          console.log(data[0]);
+          setContacts(data);
+        }
+      }
+    })();
+  }, []);
+
+  const renderItem = ({ item }) => <Item title={item.firstName} />;
+  const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, padding: 16 }}>
-        <View
-          style={{
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 20,
-              textAlign: "center",
-              marginBottom: 16,
-            }}
-          >
-            Example of Splash, Login and Sign Up in React Native
-            {"\n\n"}
-            This is the Home Screen
-          </Text>
-        </View>
-        <Text
-          style={{
-            fontSize: 18,
-            textAlign: "center",
-            color: "grey",
-          }}
-        >
-          Splash, Login and Register Example{"\n"}React Native
-        </Text>
-        <Text
-          style={{
-            fontSize: 16,
-            textAlign: "center",
-            color: "grey",
-          }}
-        >
-          www.aboutreact.com
-        </Text>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList data={contacts} renderItem={renderItem} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
 
 export default HomeScreen;
